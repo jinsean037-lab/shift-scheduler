6a374dc9abd597362c3c1d2a31c9c3abf01a5d53
jinsean037-lab <jinsean037@gmail.com>
2026-09-15 22:30:25 +0800

    Initialize app with global data and user storage

---
+++ miniprogram-member/app.js
@@ -0,0 +1,16 @@
+App({
+  globalData: {
+    apiBase: 'https://shift-scheduler-9316.onrender.com/api',
+    user: null,
+    subscribeTemplateIds: {
+      beforeShift: '',
+      missedCheckin: '',
+      missedCheckout: '',
+      exchangeNotice: ''
+    }
+  },
+  onLaunch() {
+    const user = wx.getStorageSync('memberUser')
+    if (user && user.name) this.globalData.user = user
+  }
+})
