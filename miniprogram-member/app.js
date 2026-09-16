28dc7998e6a4e465edd5932b4b36f36dca723ec5
jinsean037-lab <jinsean037@gmail.com>
2026-09-16 23:11:31 +0800

    Fix template IDs for notifications

--- miniprogram-member/app.js
+++ miniprogram-member/app.js
@@ -4,9 +4,9 @@ App({
     user: null,
     subscribeTemplateIds: {
       shiftSwap: 'sUpPazzV67bnvZAaeHMOYznai9GOZ_TRLL2dJYrfBuM',
-      shiftSubstitute: '6hP43mHFnHj9yHLgClg8YMIDFW31YqTtBxYf7CUxkW8',
-      missedCheckout: 'tAPP0R-7ZnsS5NshiCLGfKEgpN6YXlguQBl494uo7_w',
-      missedCheckin: 'Fq6xyq1ox9ils8xo8LCbql9Ek-2wC_sZryVOrY3XWaI'
+      shiftSubstitute: '6hP43mHFnHj9yHLgClg8YMlDFW31YqTtBxYf7CUxkW8',
+      missedCheckout: 'tAPP0R-7ZnsS5NshiCLGfKEgpN6YXlguQBI494uo7_w',
+      missedCheckin: 'Fq6xyq1ox9iIs8xo8LCbql9Ek-2wC_sZryVOrY3XWaI'
     }
   },
   onLaunch() {
